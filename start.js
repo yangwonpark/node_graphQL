121c48d810d52aba16b04892ad70e64c34481488
yangwonpark <ailey@nate.com>
2021-05-15 13:58:43 +0900

    1. graphql basic

---
+++ start.js
@@ -0,0 +1,18 @@
+
+const { graphql, buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+    type Query {
+        hello: String,
+        nodejs: Int
+    }
+`);
+
+const root = {
+    hello: () => 'hello world',
+    nodejs: () => 20
+}
+
+graphql(schema, '{ hello, nodejs}', root).then( (response) => {
+    console.log(response);
+});
